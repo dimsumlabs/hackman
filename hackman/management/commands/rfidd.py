a54e62bc9491426459bad649b43a8058809f6913
adisbladis <adis@blad.is>
2017-03-02 23:56:25 +0800

    Actually send CARD_UNPAIRED event

--- hackman/management/commands/rfidd.py
+++ hackman/management/commands/rfidd.py
@@ -1,7 +1,10 @@
+from hackman_notifier import api as notification_api
 from django.core.management.base import BaseCommand
 
 from hackman_rfid import api as rfid_api
 from hackman import api as hackman_api
+
+import json
 
 
 class Command(BaseCommand):
@@ -11,6 +14,13 @@ class Command(BaseCommand):
 
             card = rfid_api.card_validate(card_hash)
             if not card:
+                continue
+
+            if not card.user_id:
+                notification_api.notify_subject(b'door_event', json.dumps({
+                    'event': 'CARD_UNPAIRED',
+                    'card_id': card.id
+                }))
                 continue
 
             hackman_api.door_open_if_paid(card.user_id)
