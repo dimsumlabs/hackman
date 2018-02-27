8756dbbd27d18c2caf0cc4ec20cb19a7ed73dec4
adisbladis <adis@blad.is>
2018-02-27 21:45:11 +0800

    Add redis socket timeouts

--- hackman/settings.py
+++ hackman/settings.py
@@ -154,6 +154,8 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_CONNECT_TIMEOUT": 5,  # in seconds
+            "SOCKET_TIMEOUT": 5,  # in seconds
         }
     }
 }
