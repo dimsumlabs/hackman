d99d170910d0b0d08235c45b7f3f707d4a5dc300
Hamish Coleman <hamish@zot.org>
2018-06-22 16:44:48 +1000

    Fix flake8 complaints

--- hackman/settings_prod.py
+++ hackman/settings_prod.py
@@ -67,5 +67,3 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-
