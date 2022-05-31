f0b6d11c18d3a2cccdf33920306eba46c33bf588
adisbladis <adisbladis@gmail.com>
2022-06-01 02:59:48 +0800

    Add redis setup hooks for pytest

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import subprocess
+import typing
+
+
+p: typing.Optional[subprocess.Popen] = None
+
+
+def pytest_sessionstart(session):
+    global p
+    p = subprocess.Popen(["redis-server", "--save", "", "--port", "31337"])
+
+
+def pytest_sessionfinish(session, exitstatus):
+    global p
+    if not p:  # Make type checker happy
+        raise ValueError("Process was None")
+    p.kill()
--- hackman/settings_test.py
+++ hackman/settings_test.py
@@ -9,3 +9,15 @@ DOOR_LOCK = {
     'BACKEND': 'hackman_door.door.impls.dummy',
     'BIND_URI': 'inproc://doord_upstream',
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:31337/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_CONNECT_TIMEOUT": 5,  # in seconds
+            "SOCKET_TIMEOUT": 5,  # in seconds
+        }
+    }
+}
