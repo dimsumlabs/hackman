19b2e41d68cbd06aa615d3857a6b1b0c489e8864
adisbladis <adisbladis@gmail.com>
2022-08-13 04:53:21 +0800

    Use gevent for wsgi application

--- hackman/wsgi.py
+++ hackman/wsgi.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from gevent import monkey
+
+monkey.patch_all()
+
+from django.core.wsgi import get_wsgi_application  # noqa:E402
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackman.settings")
 
