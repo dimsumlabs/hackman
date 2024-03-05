b23f86fef32443a84da58ffb0c26a9403c79a437
Gabriel Y C Chan <gabriel@alternative-enterprises.com.hk>
2024-03-06 00:24:31 +0800

    updated README for deployment instructions fix password reset path

--- hackman/settings_prod.py
+++ hackman/settings_prod.py
@@ -77,4 +77,6 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", default="-1"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 CSRF_TRUSTED_ORIGINS = ["https://door.dimsumlabs.com"]
--- hackman/urls.py
+++ hackman/urls.py
@@ -56,8 +56,8 @@ urlpatterns = [
         auth_views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
-    re_path(
-        "reset/(?P<uidb64>[0-9A-Za-z_-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",  # noqa
+    path(
+        "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
