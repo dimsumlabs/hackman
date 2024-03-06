f8bb67f4f49d3499f7e8c1770ed0e1ef83bb3a04
Gabriel Y C Chan <gabriel@alternative-enterprises.com.hk>
2024-03-06 10:31:25 +0800

    Fix password reset path
    Closes #63

--- hackman/urls.py
+++ hackman/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
         name="password_reset_done",
     ),
     re_path(
-        "reset/(?P<uidb64>[0-9A-Za-z_-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",  # noqa
+        "reset/(?P<uidb64>[0-9A-Za-z_-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
